3e1bc44edcfde6fdf837defb9c1f88b9c041f386
Woke <66000596+woke999@users.noreply.github.com>
2021-03-10 13:24:18 -0500

    Add files via upload

---
+++ Code/Lib.cs
@@ -0,0 +1,22 @@
+﻿//injecting dlls is usually in c++, but here we have a c# library that we want to inject
+//a normal c++ dll has a DllMain which gets excecuted when it is executed, but c# does not have that
+//there isnt really an equivalent but we can get around that by calling our class 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+
+namespace SkidClientLibrary
+{
+    class Lib
+    {
+        static Lib()
+        {
+            //here you would write all your code 
+            MessageBox.Show("This was an injected dll test");
+        }
+    }
+}
